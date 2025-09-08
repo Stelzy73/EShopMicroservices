56dafec7230f8d412e09220b8c82c18396366be1
Andreas Stelzner <Andreas.Stelzner@digitalfabrix.de>
2025-09-08 13:36:03 +0200

    Ab hier weiter mit 55

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,9 +1,22 @@
+// Before Building Web Application
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add Services to the container.
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
 
 var app = builder.Build();
 
+
+
+
+// After Building Web Application
+
 // Configure the HTTP request pipeline.
+app.MapCarter();
 
 app.Run();
