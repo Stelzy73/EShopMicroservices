e4981fe627843457b33dca2cd398952170d167bd
Andreas Stelzner <Andreas.Stelzner@digitalfabrix.de>
2025-10-31 09:39:38 +0100

    Kapitel 90 Ok

---
+++ src/BuildingBlocks/BuildingBlocks/Exceptions/Handler/CustomExceptionHandler.cs
@@ -0,0 +1,57 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace BuildingBlocks.Exceptions.Handler;
+
+public class CustomExceptionHandler(ILogger<CustomExceptionHandler> logger) : IExceptionHandler
+{
+    public async ValueTask<bool> TryHandleAsync(HttpContext context, Exception exception, CancellationToken cancellationToken)
+    {
+        logger.LogError("Error Message: {exceptionMessage}, Time of occurence: {time}", exception.Message, DateTime.UtcNow);
+
+        var statusCode = exception switch
+        {
+            InternalServerException =>
+            (
+                context.Response.StatusCode = StatusCodes.Status500InternalServerError
+            ),
+            ValidationException =>
+            (
+                context.Response.StatusCode = StatusCodes.Status400BadRequest
+            ),
+            BadRequestException =>
+            (
+                context.Response.StatusCode = StatusCodes.Status400BadRequest
+            ),
+            NotFoundException =>
+            (
+                context.Response.StatusCode = StatusCodes.Status404NotFound
+            ),
+            _ =>
+            (
+                context.Response.StatusCode = StatusCodes.Status500InternalServerError
+            )
+        };
+
+        var problemDetails = new ProblemDetails
+        {
+            Title = exception.Message,
+            Detail = exception.GetType().Name,
+            Status = statusCode,
+            Instance = context.Request.Path
+        };
+
+        problemDetails.Extensions.Add("traceId", context.TraceIdentifier);
+
+        if (exception is ValidationException validationException)
+        {
+            problemDetails.Extensions.Add("ValidationErrors", validationException.Errors);
+        }
+
+        await context.Response.WriteAsJsonAsync(problemDetails, cancellationToken: cancellationToken);
+        return true;
+    }
+}
