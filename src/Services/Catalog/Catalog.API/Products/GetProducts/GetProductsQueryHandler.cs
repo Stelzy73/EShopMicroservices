f86b3706e7d096b01105b9aa962ef277cdef050f
Andreas Stelzner <Andreas.Stelzner@digitalfabrix.de>
2026-01-12 13:29:03 +0100

    Kapitel 97 - Paging

--- src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsQueryHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsQueryHandler.cs
@@ -1,16 +1,20 @@
 ﻿namespace Catalog.API.Products.GetProducts;
 
-public record GetProductsQuery() : IQuery<GetProductsResult>;
+public record GetProductsQuery(int? PageNumber = 1, int? PageSize = 10) 
+    : IQuery<GetProductsResult>;
 
-public record GetProductsResult(IReadOnlyList<Product> Products);
-
+public record GetProductsResult(IEnumerable<Product> Products);
 
 internal class GetProductsQueryHandler(IDocumentSession session)
     : IQueryHandler<GetProductsQuery, GetProductsResult>
 {
     public async Task<GetProductsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
     {
-        var products = await session.Query<Product>().ToListAsync(cancellationToken);
+        var pageNumber = query.PageNumber ?? 1;
+        var pageSize = query.PageSize ?? 10;
+        
+        var products = await session.Query<Product>()
+            .ToPagedListAsync(pageNumber, pageSize, cancellationToken);
 
         return new GetProductsResult(products);
     }
