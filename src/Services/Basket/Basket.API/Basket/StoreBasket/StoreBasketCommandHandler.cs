7c1f0ae7bdadf5bb1f83b631ad0ada9f9f2be68a
Andreas Stelzner <Andreas.Stelzner@digitalfabrix.de>
2026-01-13 11:08:35 +0100

    Kapitel 112 - Store Basket

--- src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketCommandHandler.cs
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketCommandHandler.cs
@@ -1,6 +1,18 @@
 ﻿namespace Basket.API.Basket.StoreBasket;
 
-public class StoreBasketCommandHandler
+public record StoreBasketCommand(ShoppingCart Cart) : ICommand<StoreBasketResult>;
+
+public record StoreBasketResult(string UserName);
+
+public class StoreBasketCommandHandler : ICommandHandler<StoreBasketCommand, StoreBasketResult>
 {
-    
+    public async Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
+    {
+        var cart = command.Cart;
+
+        // Todo: Store the basket in the database (use Marten upsert - if exist = update, if not = insert)
+        // Todo: Update cache
+
+        return new StoreBasketResult("swn");
+    }
 }
---
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketCommandValidator.cs
@@ -0,0 +1,10 @@
+﻿namespace Basket.API.Basket.StoreBasket;
+
+public class StoreBasketCommandValidator : AbstractValidator<StoreBasketCommand>
+{
+    public StoreBasketCommandValidator()
+    {
+        RuleFor(command => command.Cart).NotNull().WithMessage("Cart is required");
+        RuleFor(command => command.Cart.UserName).NotEmpty().WithMessage("UserName is required");
+    }
+}
