51bc0a715d194aa8b48ba57bd9177be499b3231e
Andreas Stelzner <Andreas.Stelzner@digitalfabrix.de>
2026-01-13 11:23:12 +0100

    Kapitel 114 - Delete Basket

--- src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketCommandHandler.cs
+++ src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketCommandHandler.cs
@@ -1,6 +1,16 @@
 ﻿namespace Basket.API.Basket.DeleteBasket;
 
-public class DeleteBasketCommandHandler
+public record DeleteBasketCommand(string UserName) : ICommand<DeleteBasketResult>;
+
+public record DeleteBasketResult(bool IsSuccess);
+
+public class DeleteBasketCommandHandler : ICommandHandler<DeleteBasketCommand, DeleteBasketResult>
 {
-    
+    public async Task<DeleteBasketResult> Handle(DeleteBasketCommand command, CancellationToken cancellationToken)
+    {
+        // Todo: Delete the basket from the database and cache
+        // session.Delete<ShoppingCart>(command.UserName);
+        
+        return new DeleteBasketResult(true);
+    }
 }
---
+++ src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketCommandValidator.cs
@@ -0,0 +1,9 @@
+﻿namespace Basket.API.Basket.DeleteBasket;
+
+public class DeleteBasketCommandValidator : AbstractValidator<DeleteBasketCommand>
+{
+    public DeleteBasketCommandValidator()
+    {
+        RuleFor(command => command.UserName).NotEmpty().WithMessage("UserName is required");
+    }
+}
